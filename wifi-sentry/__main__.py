f016d4cd22be1e4cbbc5b2210a704de3198af6c8
Max Loiacono <max.loiacono@protonmail.com>
2025-04-24 08:10:43 -0400

    Basic app config

--- wifi-sentry/__main__.py
+++ wifi-sentry/__main__.py
@@ -2,13 +2,24 @@
 
 import argparse
 from . import APP_TITLE, IFACE_NAME
+from .config import WifiSentryConfig
 
 
 # --- CLI Args
 parser = argparse.ArgumentParser(
-	prog=APP_TITLE
+    prog=APP_TITLE
+)
+parser.add_argument(
+    "--known-hosts",
+	"-k",
+    type=str,
+    metavar="FILE",
+    help="Path to the file containing known hosts"
 )
 args = parser.parse_args()
+
+# --- App Context Config
+config = WifiSentryConfig(IFACE_NAME, args.known_hosts)
 
 
 print("Running on", IFACE_NAME)
